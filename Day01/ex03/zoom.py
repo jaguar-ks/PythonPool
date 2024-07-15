67a4eca06b4b187da02df06a66e2807f01386dd9
jaguar-ks <fahdamineksouss@gmail.com>
2024-07-15 17:39:47 +0100

    UPDATE | PROGRESS : the image is scaled and croped succefully

--- Day01/ex03/zoom.py
+++ Day01/ex03/zoom.py
@@ -1,12 +1,19 @@
 import numpy as np
+import scipy as sp
 import matplotlib.pyplot as plt
 from load_image import ft_load
-from scipy.ndimage import zoom
 import sys
+import imageio.v3 as iio
 
 
 def zoom_img(img: np.ndarray, factor: int) -> np.ndarray:
     try:
+        f = img
+        # iio.imwrite('/Users/faksouss/Desktop/PythonPool/Day01/ex03/face.png', f)
+        n_f = sp.ndimage.zoom(f, zoom=(1.5, 1.5, 1.0))
+        n_f = n_f[100:900, 100:900]
+        plt.imshow(n_f, vmin=0, vmax=255)
+        plt.show()
         pass
     except Exception as e:
         print('Error:', e)
