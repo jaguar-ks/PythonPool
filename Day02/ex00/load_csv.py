c989583a6892c993d217a42a073973d0491e3863
jaguar-ks <fahdamineksouss@gmail.com>
2024-07-27 00:55:47 +0100

    DONE : the first exercice is done normed and documented

--- Day02/ex00/load_csv.py
+++ Day02/ex00/load_csv.py
@@ -1,13 +1,21 @@
 import pandas as pd
-import numpy as np
 import sys
 
 
 def load(path: str) -> pd.DataFrame:
+    """
+    Load a CSV file into a pandas DataFrame.
+
+    Parameters:
+        path (str): The path to the CSV file.
+
+    Returns:
+        pd.DataFrame: The loaded dataset as a pandas DataFrame,
+        or None if an error occurs.
+    """
     try:
         ds: pd.DataFrame = pd.read_csv(path)
         print('Loading a dataset of dimensions of', ds.shape)
-        
         return ds
     except Exception as e:
         print('LoadError:', e)
@@ -15,12 +23,20 @@ def load(path: str) -> pd.DataFrame:
 
 
 def main() -> None:
+    """
+    Entry point of the program.
+
+    This function loads a CSV file specified as a command line
+    argument and prints its contents.
+    If any error occurs during the loading process, an error
+    message is printed.
+    """
     try:
         if len(sys.argv) != 2:
-            raise ValueError('Invalide number of arguments!')
+            raise ValueError('Invalid number of arguments!')
         ds = load(sys.argv[1])
         if ds is None or ds.empty:
-            raise ValueError('load error accured!')
+            raise ValueError('Load error occurred!')
         print(ds)
     except Exception as e:
         print('Error:', e)
