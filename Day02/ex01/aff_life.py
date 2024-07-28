7610bf350ca14ce6e4214b7b1ad2d7a6a929e9b1
jaguar-ks <fahdamineksouss@gmail.com>
2024-07-28 11:49:59 +0100

    UPDATE : removed some comments

--- Day02/ex01/aff_life.py
+++ Day02/ex01/aff_life.py
@@ -8,37 +8,29 @@ import seaborn as sns
 
 def renderGraph(ds: pd.DataFrame) -> None:
     """
-    Render a graph of the dataset.
+    Renders a line graph of life expectancy in Morocco over the years.
 
-    Parameters:
-        ds (pd.DataFrame): The dataset to render.
+    Args:
+        ds (pd.DataFrame): The input DataFrame containing life expectancy data.
+
+    Returns:
+        None
     """
     try:
-        # Transpose the DataFrame if not already done
         ds = ds.T
-
-        # Reset index to make 'Years' a column
         ds.reset_index(inplace=True)
         ds.rename(columns={'index': 'Year'}, inplace=True)
-
-        # Convert index to numeric if necessary (optional)
         ds['Year'] = pd.to_numeric(ds['Year'], errors='coerce')
-
-        # Plot the data
         plt.figure(figsize=(10, 6))
         sns.lineplot(x='Year', y='Morocco', data=ds).set(
             xlabel='Years',
             ylabel='Life expectancy',
             title='Morocco life expectancy projection'
         )
-
-        # Customize the x-axis ticks
         plt.xticks(ticks=np.arange(1800, 2100, 20), rotation=45)
-
-        # Display the plot
         plt.show()
     except Exception as e:
-        print('Rendring the Graph:', e)
+        print('Rendering the Graph:', e)
 
 
 if __name__ == '__main__':
