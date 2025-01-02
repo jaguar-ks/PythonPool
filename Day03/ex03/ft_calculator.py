5aaccf59784f6a713014c178e739824b9daeff3d
jaguar-ks <fahdamineksouss@gmail.com>
2025-01-02 05:15:35 +0100

    DONE : exercice done

--- Day03/ex03/ft_calculator.py
+++ Day03/ex03/ft_calculator.py
@@ -1,5 +1,20 @@
 
 class calculator:
+    """
+    A class representing a calculator.
+
+    Attributes:
+    - vector: A list of floats representing the vector.
+
+    Methods:
+    - __init__(self, vector: list[float]): Initializes the calculator with a vector.
+    - __add__(self, object) -> None: Adds a value to each element in the vector.
+    - __sub__(self, object) -> None: Subtracts a value from each element in the vector.
+    - __mul__(self, object) -> None: Multiplies each element in the vector by a value.
+    - __truediv__(self, object) -> None: Divides each element in the vector by a value.
+
+    """
+
     def __init__(self, vector: list[float]):
         self.vector = vector
     
@@ -26,13 +41,13 @@ class calculator:
         else:
             print("Error: Division by zero")
 
-if __name__ == "__main__":
-    v1 = calculator([0.0, 1.0, 2.0, 3.0, 4.0, 5.0])
-    v1 + 5
-    print("---")
-    v2 = calculator([0.0, 1.0, 2.0, 3.0, 4.0, 5.0])
-    v2 * 5
-    print("---")
-    v3 = calculator([10.0, 15.0, 20.0])
-    v3 - 5
-    v3 / 5
+# if __name__ == "__main__":
+#     v1 = calculator([0.0, 1.0, 2.0, 3.0, 4.0, 5.0])
+#     v1 + 5
+#     print("---")
+#     v2 = calculator([0.0, 1.0, 2.0, 3.0, 4.0, 5.0])
+#     v2 * 5
+#     print("---")
+#     v3 = calculator([10.0, 15.0, 20.0])
+#     v3 - 5
+#     v3 / 5
