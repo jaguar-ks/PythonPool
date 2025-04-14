a130397f664a062c375f5df197d07a616c79e9bb
jaguar-ks <fahdamineksouss@gmail.com>
2025-04-14 17:42:05 +0100

    REFACTOR : recoded the whole renderGraph function and made an update to the load csv file function

--- Day02/ex02/aff_pop.py
+++ Day02/ex02/aff_pop.py
@@ -2,37 +2,36 @@ import pandas as pd
 from load_csv import load
 import sys
 import matplotlib.pyplot as plt
-import numpy as np
 
 
 def renderGraph(ds: pd.DataFrame) -> None:
     """
-    Renders a graph of life expectancy over the years for
-    Belgium and Morocco.
+    Renders a graph of Population over the years for
+    France and Morocco.
 
     Args:
         ds (pd.DataFrame): The input DataFrame containing
-        life expectancy data.
+        population data.
 
     Returns:
         None
     """
     try:
-        ds = ds.T
-        ds.reset_index(inplace=True)
-        ds.rename(columns={'index': 'Year'}, inplace=True)
-        ds['Year'] = pd.to_numeric(ds['Year'], errors='coerce')
-        plt.yticks(ticks=np.arange(min(ds['Belgium']),
-                                   max(ds['Belgium']),
-                                   20),
-                   rotation=45)
-        plt.xticks(ticks=np.arange(1800, 2050, 40))
-        plt.xlabel('Year')
-        plt.ylabel('Life Expectancy')
-        plt.plot(ds['Year'], ds['Belgium'], label='Belgium')
-        plt.plot(ds['Year'], ds['Morocco'], label='Morocco')
-        plt.legend()
-        plt.show()
+        nds = ds.loc[ds.country.isin(['Morocco', 'France']),
+                     '1800':'2050']
+        nds = nds.T
+        nds.reset_index(inplace=True)
+        nds.rename(columns={'index': 'Year'}, inplace=True)
+        nds.rename(columns={58: 'France'}, inplace=True)
+        nds.rename(columns={106: 'Morocco'}, inplace=True)
+        nds['Year'] = pd.to_numeric(nds['Year'], errors='coerce')
+        nds.plot.line(x='Year',
+                      ylabel='Population',
+                      title='Population over the years of France and Morocco'
+                      )
+        # plt.show()
+        plt.savefig("plot.png")
+        # print(nds)
         pass
     except Exception as e:
         print('Rendering the Graph:', e)
