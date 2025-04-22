1b53fd83381cfd6a46f873bfdbbd1b6996aade39
jaguar-ks <fahdamineksouss@gmail.com>
2025-04-22 23:28:26 +0100

    UPDATE : Used seaborn library to generate the scatter graph instead of pyplot and saved it as an image instead of showing it

--- Day02/ex03/projection_life.py
+++ Day02/ex03/projection_life.py
@@ -2,6 +2,7 @@ from load_csv import load
 import sys
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 def renderGraph(gdp: pd.DataFrame, le: pd.DataFrame) -> None:
@@ -14,23 +15,23 @@ def renderGraph(gdp: pd.DataFrame, le: pd.DataFrame) -> None:
         life expectancy.
     """
     try:
-        plt.figure(figsize=(12, 6))
-        plt.scatter(gdp['1900'], le['1900'])
+        plt.figure(figsize=(8, 6))
+        sns.scatterplot(x=gdp['1900'], y=le['1900'])
         plt.xscale('log')
+        plt.title('1900')
         plt.xlabel('Gross domestic product')
         plt.ylabel('Life Expectancy')
-        plt.title('1900')
-        plt.xticks([300, 1000, 3000, 10000], ['300', '1k', '3k', '10k'])
-        plt.xlim(300, 10000)
-        plt.ylim(20, 55)
-        plt.show()
+        plt.xticks([300, 1000, 10000], ['300', '1k', '10k'])
+        plt.xlim(300, 10100)
+        # plt.show()
+        plt.savefig('scatterPlot.png')
     except Exception as e:
         print('Rendering the Graph:', e)
 
 
 if __name__ == '__main__':
     try:
-        gdp = load('income_per_person_gdppercapita_ppp_inflation_adjusted.csv')
+        gdp = load('pop.csv')
         le = load('life_expectancy_years.csv')
         renderGraph(gdp, le)
     except Exception as e:
