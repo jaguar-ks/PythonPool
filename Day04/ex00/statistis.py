d2266626472b9eefd7551f67ff0168c5811669ea
jaguar-ks <fahdamineksouss@gmail.com>
2025-05-16 23:34:43 +0100

    INIT | UPDATE : working on the DoD day the first exercice almost done

---
+++ Day04/ex00/statistis.py
@@ -0,0 +1,23 @@
+from typing import Any
+import numpy as np
+import pandas as pd
+
+
+def ft_statistics(*args: Any, **kwargs: Any) -> None:
+    ds = pd.Series(args)
+    for k in kwargs.values():
+        if ds.empty:
+            print('ERROR')
+            continue
+        if hasattr(ds, k):
+            method = getattr(ds, k)
+            print(k, ':', method())
+
+if __name__ == '__main__':
+    ft_statistics(1, 42, 360, 11, 64, toto="mean", tutu="median", tata="quartile")
+    print("-----")
+    ft_statistics(5, 75, 450, 18, 597, 27474, 48575, hello="std", world="var")
+    print("-----")
+    ft_statistics(5, 75, 450, 18, 597, 27474, 48575, ejfhhe="heheh", ejdjdejn="kdekem")
+    print("-----")
+    ft_statistics(toto="mean", tutu="median", tata="quartile")
