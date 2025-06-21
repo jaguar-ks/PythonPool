786704043d9526c19f3408e40fbfe1a38b9f3dd3
jaguar-ks <fahdamineksouss@gmail.com>
2025-06-21 16:26:19 +0100

    UPDATE : first exercice is done

--- Day04/ex00/statistis.py
+++ Day04/ex00/statistis.py
@@ -5,10 +5,14 @@ import pandas as pd
 
 def ft_statistics(*args: Any, **kwargs: Any) -> None:
     ds = pd.Series(args)
+    d = ds.describe()
     for k in kwargs.values():
         if ds.empty:
             print('ERROR')
             continue
+        if k=='quartile':
+            print(k, ':', d.loc[['25%', '75%']].to_list())
+            pass
         if hasattr(ds, k):
             method = getattr(ds, k)
             print(k, ':', method())
